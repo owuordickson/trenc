8f2e9dce84dc8bce8dc7bc15906665ea49588b41
owuordickson <owuordickson@gmail.com>
2020-03-11 21:49:20 +0300

    added

---
+++ setup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+with open('requirements.txt') as requirements_file:
+    requirements = requirements_file.read()
+
+test_requirements = [
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='trenc',
+    version='1.0',
+    description="A Python implementation of TRENC algorithm.",
+    long_description=readme + '\n\n' + history,
+    author="Dickson Owuor",
+    author_email='owuordickson@ieee.org',
+    url='https://github.com/owuordickson/trenc',
+    packages=find_packages(),
+    # package_dir={'tgraank':'tgraank'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="MIT",
+    zip_safe=False,
+    keywords='emerging patterns',
+    classifiers=[
+        'Development Status :: 1 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Massachusetts Institute of Technology (MIT) License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements
+)
