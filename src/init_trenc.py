5e357f37d2f11342a17b48886dce9162781d0fb0
owuordickson <owuordickson@gmail.com>
2020-03-23 19:07:10 +0300

    testing

--- src/init_trenc.py
+++ src/init_trenc.py
@@ -2,21 +2,21 @@
 from src.algorithms.trenc.trenc import Trenc
 
 
-def init_trenc(paths, min_sup=0.5, cores=0, allow_para=0, min_rep=None, ref_item=0):
+def init_trenc(paths, min_sup=0.5, cores=0, allow_para=0, min_rep=0.5, ref_item=1):
     wr_line = ""
     # ep_set = Trenc(paths, 0.5)
-    ep_set = Trenc(paths, 0.5, cores=0, allow_para=1, min_rep=0.5, ref_item=1)
+    ep_set = Trenc(paths, 0.5, cores, allow_para, min_rep, ref_item)
     # print(ep_set.d_sets)
     ep_set.run_trenc()
 
     titles = ep_set.titles
     if ref_item is not None:
         for txt in titles:
-            col = (int(txt[0]) - 0)
+            col = (int(txt[0]) - 1)
             if col == ref_item:
-                wr_line += (str(txt[0]) + '. ' + str(txt[1]) + '**' + '\n')
+                wr_line += (str(col) + '. ' + str(txt[1]) + '**' + '\n')
             else:
-                wr_line += (str(txt[0]) + '. ' + str(txt[1]) + '\n')
+                wr_line += (str(col) + '. ' + str(txt[1]) + '\n')
     else:
         for txt in titles:
             wr_line += (str(txt[0]) + '. ' + str(txt[1]) + '\n')
