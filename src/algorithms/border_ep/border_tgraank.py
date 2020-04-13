fa5fd635921de6d8f24473f2cfe91b09260f8400
owuordickson <owuordickson@gmail.com>
2020-04-13 21:32:23 +0300

    added

--- src/algorithms/border_ep/border_tgraank.py
+++ src/algorithms/border_ep/border_tgraank.py
@@ -1,24 +1,4 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Fri Jun 12 14:31:16 2015
-
-@author: Olivier + modif MJL+MR 140316
-
-Modified on Tue Oct 23 2018 by Dickson Owuor
-@credits: "Anne Laurent, Joseph Orero"
-
-This code (originally) correlates gradual patterns using concordant pairs
-We added a functionality that allows for retrieval of time lags from concordant
-indices, approximation using a fuzzy membership function, discovery of emerging
-fuzzy-temporal gradual patterns
-
-
-BorderT-GRAANK - denotes Border Temporal GRAANK (GRAdual rANKing)
-
-"""
-
-# -*- coding: utf-8 -*-
-import sys
 
 """
 Created on Fri Jun 12 14:31:16 2015
@@ -40,10 +20,10 @@ Usage:
 
 import numpy as np
 import gc
-from optparse import OptionParser
-from fuzzy_temporal import init_fuzzy_support
-from data_transform import DataTransform
-from mbdll_border import *
+# from optparse import OptionParser
+from src.algorithms.border_ep.fuzzy_temporal import init_fuzzy_support
+from src.algorithms.border_ep.data_transform import DataTransform
+# from src.algorithms.border_ep.mbdll_border import *
 
 
 def trad(dataset):
@@ -391,132 +371,3 @@ def get_maximal_items(init_list, tlag_list):
 
 # --------------------- EXECUTE BORDER T-GRAANK ----------------------------------------------
 
-
-def algorithm_ep_init(filename, ref_item, minsup, minrep):
-    try:
-        fgp_list = list()  # fuzzy-temporal gradual patterns
-
-        # 1. Load dataset into program
-        dataset = DataTransform(filename, ref_item, minrep)
-
-        # 2. TRANSFORM DATA (for each step)
-        for s in range(dataset.max_step):
-            step = s+1  # because for-loop is not inclusive from range: 0 - max_step
-            # 3. Calculate representativity
-            chk_rep, rep_info = dataset.get_representativity(step)
-
-            if chk_rep:
-                # 4. Transform data
-                data, time_diffs = dataset.transform_data(step)
-
-                # 5. Execute GRAANK for each transformation
-                title, gp_list, sup_list, tlag_list = graank(trad(list(data)), minsup, time_diffs, eq=False)
-
-                pattern_found = check_for_pattern(ref_item, gp_list)
-                if pattern_found:
-                    maximal_items = get_maximal_items(gp_list, tlag_list)
-                    fgp_list.append(tuple((title, maximal_items)))
-        if not fgp_list:
-            print("Oops! no frequent patterns were found")
-            print("----------------------------------------------------------------")
-        else:
-            print("Total Data Transformations: " + str(dataset.max_step) + " | " + "Minimum Support: " + str(min_sup))
-            print("----------------------------------------------------------------")
-            for line in title:
-                print(line)
-            print('Emerging Pattern | Time Lags: (Transformation n, Transformation m)')
-
-            all_fgps = list()
-            for item_list in fgp_list:
-                for item in item_list[1]:
-                    all_fgps.append(item)
-
-            patterns = 0
-            ep_list = list()
-            for i in range(len(all_fgps)):
-                for j in range(i, len(all_fgps)):
-                    if i != j:
-                        freq_pattern_1 = all_fgps[i]
-                        freq_pattern_2 = all_fgps[j]
-                        ep = mbdll_border(tuple(freq_pattern_1[0]), tuple(freq_pattern_2[0]))
-                        tlags = tuple((freq_pattern_1[1], freq_pattern_2[1]))
-                        if ep:
-                            patterns = patterns + 1
-                            temp = tuple((ep, tlags))
-                            ep_list.append(temp)
-                            print(str(temp[0]) + " | " + str(temp[1]))
-
-            print("\nTotal: " + str(patterns) + " FtGEPs found!")
-            print("---------------------------------------------------------")
-            if patterns == 0:
-                print("Oops! no relevant emerging pattern was found")
-                print("---------------------------------------------------------")
-    except Exception as error:
-        print(error)
-
-# ------------------------- main method ----------------------------------------------------
-
-
-if __name__ == "__main__":
-
-    if not sys.argv:
-        pattern_type = sys.argv[1]
-        file_name = sys.argv[2]
-        ref_col = sys.argv[3]
-        min_sup = sys.argv[4]
-        min_rep = sys.argv[5]
-
-    else:
-        optparser = OptionParser()
-        optparser.add_option('-t', '--patternType',
-                             dest='pType',
-                             help='patterns: FtGP, FtGEP',
-                             default=1,
-                             type='int')
-        optparser.add_option('-f', '--inputFile',
-                             dest='file',
-                             help='path to file containing csv',
-                             default=None,
-                             type='string')
-        optparser.add_option('-c', '--refColumn',
-                             dest='refCol',
-                             help='reference column',
-                             default=0,
-                             type='int')
-        optparser.add_option('-s', '--minSupport',
-                             dest='minSup',
-                             help='minimum support value',
-                             default=0.7,
-                             type='float')
-        optparser.add_option('-r', '--minRepresentativity',
-                             dest='minRep',
-                             help='minimum representativity',
-                             default=0.5,
-                             type='float')
-
-        (options, args) = optparser.parse_args()
-
-        inFile = None
-        if options.file is None:
-            #inFile = 'DATASET.csv'
-            #inFile = '../misc/data/rain_temp1991-2015.csv'
-            #inFile = '../misc/data/ICU_household_power_consumption1.csv'
-            inFile = '../misc/data/ICU_household_power_consumption2.csv'
-            #inFile = '../misc/data/ICU_household_power_consumption.csv'
-
-            #print("Usage: $python t_graank.py -f filename.csv -c refColumn -s minSup  -r minRep")
-            #sys.exit('System will exit')
-        else:
-            inFile = options.file
-
-        file_name = inFile
-        pattern_type = options.pType
-        ref_col = options.refCol
-        min_sup = options.minSup
-        min_rep = options.minRep
-
-    #import timeit
-    if pattern_type == 1:
-        algorithm_init(file_name, ref_col, min_sup, min_rep)
-    else:
-        algorithm_ep_init(file_name, ref_col, min_sup, min_rep)
--- src/init_trenc.py
+++ src/init_trenc.py
@@ -1,12 +1,29 @@
 
+import sys
+from optparse import OptionParser
 from src.algorithms.trenc.trenc import Trenc
+from src.algorithms.trenc.handle_data import HandleData
 
 
-def init_trenc(paths, min_sup=0.5, ref_item=1, cores=0, allow_para=0, min_rep=0.5):
+def init_trenc(paths, min_sup, ref_item, cores, allow_para, min_rep):
     wr_line = ""
     # ep_set = Trenc(paths, 0.5)
     ep_set = Trenc(paths, min_sup, cores, allow_para, min_rep, ref_item)
+
+    if allow_para >= 1:
+        msg_para = "True"
+        # list_tgp = tgp.run_tgraank(parallel=True)
+    else:
+        msg_para = "False"
+        # list_tgp = tgp.run_tgraank()
     ep_list, jep_list = ep_set.run_trenc(0)
+
+    wr_line = "Algorithm: TRENC \n"
+    #wr_line += "No. of (dataset) attributes: " + str(d_set.column_size) + '\n'
+    #wr_line += "No. of (dataset) tuples: " + str(d_set.size) + '\n'
+    wr_line += "Multi-core execution: " + str(msg_para) + '\n'
+    #wr_line += "Number of cores: " + str(tgp.cores) + '\n'
+    #wr_line += "Number of tasks: " + str(tgp.max_step) + '\n\n'
 
     titles = ep_set.titles
     if ref_item is not None:
@@ -20,6 +37,9 @@ def init_trenc(paths, min_sup=0.5, ref_item=1, cores=0, allow_para=0, min_rep=0.
         for txt in titles:
             wr_line += (str(txt[0]) + '. ' + str(txt[1]) + '\n')
 
+    wr_line += str("\nFile: " + paths + '\n')
+    wr_line += str("Patterns" + '\n\n')
+
     if len(ep_list) < 1 and jep_list < 1:
         wr_line += 'No emerging patterns found\n'
     else:
@@ -27,12 +47,73 @@ def init_trenc(paths, min_sup=0.5, ref_item=1, cores=0, allow_para=0, min_rep=0.
             wr_line += str(ep.pattern_info) + '\n'
         for jep in jep_list:
             wr_line += str(jep.pattern_info) + '\n'
-    wr_line += '\n\n --- end --- \n\n RAW PATTERNS\n'
-    for obj in ep_set.GR_list:
-        wr_line += str(obj[1].extracted_patterns) + '\n'
-        wr_line += str(obj[2].extracted_patterns) + '\n'
-        wr_line += '\n\n'
-    print(wr_line)
+    wr_line += '\n\n --- end --- \n\n '
+    # wr_line += 'RAW PATTERNS\n'
+    # for obj in ep_set.GR_list:
+    #    wr_line += str(obj[1].extracted_patterns) + '\n'
+    #    wr_line += str(obj[2].extracted_patterns) + '\n'
+    #    wr_line += '\n\n'
+    # print(wr_line)
+    return wr_line
 
 
-init_trenc('../data/DATASET.csv')
+if __name__ == "__main__":
+    if not sys.argv:
+        file_path = sys.argv[1]
+        ref_col = sys.argv[2]
+        min_sup = sys.argv[3]
+        min_rep = sys.argv[4]
+        allow_p = sys.argv[5]
+    else:
+        optparser = OptionParser()
+        optparser.add_option('-f', '--inputFile',
+                             dest='file',
+                             help='path to file containing csv',
+                             # default=None,
+                             default='../data/DATASET.csv',
+                             type='string')
+        optparser.add_option('-c', '--refColumn',
+                             dest='refCol',
+                             help='reference column',
+                             default=1,
+                             type='int')
+        optparser.add_option('-s', '--minSupport',
+                             dest='minSup',
+                             help='minimum support value',
+                             default=0.5,
+                             type='float')
+        optparser.add_option('-r', '--minRepresentativity',
+                             dest='minRep',
+                             help='minimum representativity',
+                             default=0.98,
+                             type='float')
+        optparser.add_option('-p', '--allowMultiprocessing',
+                             dest='allowPara',
+                             help='allow multiprocessing',
+                             default=1,
+                             type='int')
+        (options, args) = optparser.parse_args()
+        inFile = None
+        if options.file is None:
+            print('No data-set filename specified, system with exit')
+            print("Usage: $python3 init_trenc.py -f filename.csv -c refColumn -s minSup  -r minRep")
+            sys.exit('System will exit')
+        else:
+            inFile = options.file
+        file_path = inFile
+        ref_col = options.refCol
+        min_sup = options.minSup
+        min_rep = options.minRep
+        allow_p = options.allowPara
+
+    import time
+    start = time.time()
+    res_text = init_trenc(file_path, min_sup, ref_col, 1, allow_p, min_rep)
+    # res_text = init_trenc('../data/DATASET.csv')  # init_algorithm(file_path, ref_col, min_sup, min_rep, allow_p)
+    end = time.time()
+
+    wr_text = ("Run-time: " + str(end - start) + " seconds\n")
+    wr_text += str(res_text)
+    f_name = str('res_trenc' + str(end).replace('.', '', 1) + '.txt')
+    # HandleData.write_file(wr_text, f_name)
+    print(wr_text)
